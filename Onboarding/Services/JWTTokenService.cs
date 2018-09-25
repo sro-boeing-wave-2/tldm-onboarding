737ed9f0d96279699a9b1fbd7f4a90c486e13ba0
Preetham D <preethamdbangera@gmail.com>
2018-09-25 12:08:53 +0530

    added network mode host

--- Onboarding/Services/JWTTokenService.cs
+++ Onboarding/Services/JWTTokenService.cs
@@ -39,7 +39,9 @@ namespace Onboarding.Services
             using (var client = new ConsulClient())
             {
                 Console.WriteLine("\n" + "hello boss2" + "\n");
-                client.Config.Address = new Uri("http://10.0.75.1:8500");
+                //client.Config.Address = new Uri("http://10.0.75.1:8500");
+                //for aws
+                client.Config.Address = new Uri("http://localhost:8500");
                 var putPair = new KVPair("secretkey")
                 {
 
