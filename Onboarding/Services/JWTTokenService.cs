3ac50f1d7343d9bacef425292d7e83b9ac3a763c
Preetham D <preethamdbangera@gmail.com>
2018-09-26 15:48:24 +0530

    added http

--- Onboarding/Services/JWTTokenService.cs
+++ Onboarding/Services/JWTTokenService.cs
@@ -59,7 +59,7 @@ namespace Onboarding.Services
                 //client.Config.Address = new Uri("http://10.0.75.1:8500");
                 //for aws
                // client.Config.Address = new Uri("http://"+html+":8500");
-                client.Config.Address = new Uri(Environment.GetEnvironmentVariable("MACHINE_LOCAL_IPV4")+":8500");
+                client.Config.Address = new Uri("http://"+Environment.GetEnvironmentVariable("MACHINE_LOCAL_IPV4")+":8500");
                 //client.Config.Address = new Uri("http://13.233.42.222:8500");
                 var putPair = new KVPair("secretkey")
                 {
