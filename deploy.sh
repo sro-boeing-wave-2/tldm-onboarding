4520e93344eb63bb0d227e1bfca9c759b1df62b9
Preetham D <preethamdbangera@gmail.com>
2018-09-26 16:39:49 +0530

    higher value for http timeout

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 echo Executing Deployment Script
-COMPOSE_HTTP_TIMEOUT = 200 docker-compose -H ${DEPLOYMENT_SERVER}:2375 up --build -d --remove-orphans
+docker-compose -H ${DEPLOYMENT_SERVER}:2375 up --build -d --remove-orphans
